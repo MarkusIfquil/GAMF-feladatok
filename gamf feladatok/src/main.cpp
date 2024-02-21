9a2c2d4d27a4fac21c77300b2de8807338780d2c
MarkusIfquil <141030681+MarkusIfquil@users.noreply.github.com>
2024-02-21 18:24:03 +0100

    Update main.cpp
    elso feladat a)

--- gamf feladatok/src/main.cpp
+++ gamf feladatok/src/main.cpp
@@ -1,6 +1,32 @@
 #include <iostream>
+#include <fstream>
 
-int main(int argc, char *argv[])
-{
-    std::cout << "Hello world!" << std::endl;
+#define MAGIC_NUM 1310438493
+using namespace std;
+
+long int gcd(long int a, long int b) {
+    long int x;
+    while (b) {
+        x = a % b;
+        a = b;
+        b = x;
+    }
+    return a;
+}
+
+int main() {
+    string Line;
+    int RelativePrimes = 0;
+    ifstream Numbers("szamok.txt");
+    while (!Numbers.eof()) {
+        getline(Numbers, Line);
+        long int number = stol(Line);
+        if (gcd(number, MAGIC_NUM) == 1) {
+            RelativePrimes++;
+        }
+    }
+
+    Numbers.close();
+    cout << RelativePrimes;
+    return 0;
 }
